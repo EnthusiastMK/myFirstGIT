ba181b0d0bcd2b47e1e2054b55679533d69a3e1a
Maksym Kovalenko <m.kovalenko.it@gmail.com>
2023-03-10 19:33:58 +0200

    First commit

---
+++ js/script.js
@@ -0,0 +1,113 @@
+"use strict";
+
+// console.log(typeof(4 + +'5'));
+
+// let incr = 10,
+//     decr = 10;
+    
+// console.log(incr++);
+// console.log(++incr);
+// console.log(incr);
+
+// console.log(decr);
+// console.log(--decr);
+// console.log(decr--);
+// console.log(decr);
+
+console.log(2*4 === 8);
+
+// console.log(48%7);
+
+//  && / ||
+
+
+const isChecked = true,
+      isClose = false;
+
+console.log(isChecked || !isChecked);
+
+
+
+//////////////////////////////////#3/////////////////////////////
+// alert('Hello');
+// let privet = confirm('Are you here?'); // true/false
+// const answer = +prompt('Nu sho tam?', 'Norm');
+// console.log(typeof(answer));
+
+
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Как ваше фамилия?', '');
+// answers[2] = +prompt('Сколько вам лет?', '');
+
+// document.write('Имя: ', answers[0], '. Фамилия: ', answers[1], '. Возраст: ', answers[2], ' лет.' );
+// const answers = [];
+// console.log(typeof(answers));
+// console.log(typeof(null));
+
+// let nameMax = 'Максим';
+// let hello = 'Привет';
+// let howAreYou = 'как дела';
+// console.log(`${hello} ${nameMax}, ${howAreYou}?`);
+
+// const category = 'toys';
+// console.log(`https://someurl.com/${category}/4`);
+
+// const user = 'Ivan';
+
+// alert(`Привет, ${user}`)
+
+/////////////////////#2/////////////////////////
+// 8 types dates
+//#1 let number = 4.6; - Number
+//#1.1 console.log(-4/0); - Infinity
+//#1.2 console.log('string' * 9); - NaN
+//#2 const persone = 'Alex'; - String
+//                 "Alex";
+//                 `Alex`;
+//#3 const bool = true/false; - Boolean
+//#4 console.log(something); - Null Не существует
+//#5 let und; // - undefined; - Пусто
+ 
+//#6 const obj = {  
+//     name: 'John',
+//     age: 25,
+//     isMarried: false
+// }
+// console.log(obj.name);
+// console.log(obj['name']);
+
+// //#7
+// let arr = ['plum.png', 'ice.jpg', 'cube.bmp', 6, {}, [], true];
+// console.log(arr, arr[0], arr[4]);
+
+/////////////////////#1////////////////////////
+// console.log(number);
+
+// let number = 5;
+
+// const leftBorderWidth = 1;
+
+// number = 10;
+// console.log(number);
+// console.log(leftBorderWidth);
+
+// const obj = {
+//     a: 50
+// };
+// console.log(obj);
+
+// obj.a = 10;
+// console.log(obj);
+
+// console.log(name);
+// var name = 'Ivan';
+// console.log(name);
+
+// name = 'Alex';
+// console.log(name);
+
+// {
+//     let result = 50;
+// }
+
+// console.log(result);
